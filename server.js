7fdc49a23972cd1c189a47657a3d0bf3cd1fe4b3
Vanessa Bou <rvbouu@gmail.com>
2024-05-11 16:43:03 -0500

    started draft for main.handlebars; updated controllers/views

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const helpers = require('./utils/helpers');
+const helpers = require('./lib/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
