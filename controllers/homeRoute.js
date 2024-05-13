919b1c2de0ab0fee37c6c73ee0b8493393f5e479
Vanessa Bou <rvbouu@gmail.com>
2024-05-12 23:26:44 -0500

    got homepage to dynamically input; linked nav bar

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,14 +1,44 @@
 const router = require("express").Router();
-const {Post, User} = require("../models")
+const { Post, User } = require("../models")
 
 // get route findall posts
-router.get("/", async (req,res) => {
+router.get("/", async (req, res) => {
   try {
-    const result = await Post.findAll()
-    res.render("homepage", { posts: result }); // Render the "homepage" view and pass the posts data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User
+        }
+      ]
+    })
+    const userData = await User.findAll()
+    const posts = postData.map((post) => post.get({ plain: true }));
+    const users = userData.map((user) => user.get({ plain: true }));
+    res.render("homepage",
+      {
+        posts,
+        users,
+        logged_in: req.session.logged_in
+      }); // Render the "homepage" view and pass the posts data
   } catch (err) {
     res.status(500).json({ status: "error", message: err.message });
   }
+})
+
+router.get('/resources', async(req, res) => {
+  res.render('resources',{
+    logged_in: req.session.logged_in
+  })
+})
+router.get('/games', async(req, res) => {
+  res.render('games',{
+    logged_in: req.session.logged_in
+  })
+})
+router.get('/profile', async(req, res) => {
+  res.render('profile',{
+    logged_in: req.session.logged_in
+  })
 })
 //render homepage
 //
