305b86e29bfd7e39bf823f8628049a2ed28c43bc
Maggie McDowell <maggiemcdowell4@gmail.com>
2024-05-13 13:04:43 -0600

    login logout

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { Post, User } = require("../models")
+const { Post, User, Following } = require("../models")
+const withAuth = require("../lib/auth");
 
 // get route findall posts
 router.get("/", async (req, res) => {
@@ -35,11 +36,60 @@ router.get('/games', async(req, res) => {
     logged_in: req.session.logged_in
   })
 })
-router.get('/profile', async(req, res) => {
-  res.render('profile',{
-    logged_in: req.session.logged_in
-  })
-})
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }, {model: Following}],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+router.get('/profile/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.user_id, {
+      attributes:{ exclude: ['password'] },
+      include: [{model: Post} , {model:Following}],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('profile', {
+      ...profile,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 //render homepage
 //
 
